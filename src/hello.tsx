85996feb4496e2e64b85b5573667d828b4a77f5a
adwd <ecdt.mn@gmail.com>
2018-02-16 08:28:58 +0900

    support scss

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
-const styles = require('./hello.css')
+import * as styles from './hello.scss';
 
-const Hello = () => <p className={styles.hello}>hello poi typescript react</p>
+const Hello = () => (
+  <div className={styles.hello}>
+    hello <span className={styles.inside}>poi typescript react</span>
+  </div>
+)
 
 export default Hello
